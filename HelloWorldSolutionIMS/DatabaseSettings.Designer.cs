3fec36dc656797df4ac515d395f742b669f9434d
Qazi Mubin <qazimubin444@gmail.com>
2021-08-25 13:20:58 +0500

    FIXED

---
+++ HelloWorldSolutionIMS/DatabaseSettings.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace HelloWorldSolutionIMS
+{
+    partial class DatabaseSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.ISCB = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(369, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(830, 83);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(236, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(441, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DATABASE CONNECTION";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Controls.Add(this.txtPassword);
+            this.panel2.Controls.Add(this.txtUsername);
+            this.panel2.Controls.Add(this.txtDatabase);
+            this.panel2.Controls.Add(this.txtServer);
+            this.panel2.Controls.Add(this.ISCB);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(369, 509);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnSave.FlatAppearance.BorderSize = 2;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(26, 427);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(314, 44);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(26, 371);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(316, 23);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(26, 309);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(316, 23);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Location = new System.Drawing.Point(26, 247);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(316, 23);
+            this.txtDatabase.TabIndex = 1;
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(26, 185);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(316, 23);
+            this.txtServer.TabIndex = 0;
+            // 
+            // ISCB
+            // 
+            this.ISCB.AutoSize = true;
+            this.ISCB.ForeColor = System.Drawing.Color.White;
+            this.ISCB.Location = new System.Drawing.Point(26, 400);
+            this.ISCB.Name = "ISCB";
+            this.ISCB.Size = new System.Drawing.Size(188, 21);
+            this.ISCB.TabIndex = 4;
+            this.ISCB.Text = "INTEGRATED SECURITY";
+            this.ISCB.UseVisualStyleBackColor = true;
+            this.ISCB.CheckedChanged += new System.EventHandler(this.ISCB_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(23, 351);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "PASSWORD";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(25, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "USERNAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(23, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "DATABASE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(23, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "SERVER";
+            // 
+            // DatabaseSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1199, 509);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "DatabaseSettings";
+            this.Text = "DatabaseSettings";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.CheckBox ISCB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
--- HelloWorldSolutionIMS/MDI.cs
+++ HelloWorldSolutionIMS/MDI.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,8 +21,123 @@ namespace HelloWorldSolutionIMS
 
         private void MDI_Load(object sender, EventArgs e)
         {
-            LoginScreen ls = new LoginScreen();
-            MainClass.showWindow(ls,this);
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (!File.Exists(path + "\\myconnect"))
+            {
+                DatabaseSettings sl = new DatabaseSettings();
+                sl.ShowDialog();
+            }
+            long fileLen = new FileInfo(path + "\\myconnect").Length;
+            if (File.Exists(path + "\\myconnect") && fileLen != 0)
+            {
+                try
+                {
+                    MainClass.con.Open();
+                    SqlCommand cmd = new SqlCommand("select * from example where status = 1 ", MainClass.con);
+                    cmd.CommandType = CommandType.Text;
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        if (dr[2].ToString() == "1")
+                        {
+                            dr.Close();
+                            SqlCommand cmd1 = new SqlCommand("select left(e.Keys,5)+'-' +RIGHT(e.Keys,4) as 'Date' from example e where e.status = 1", MainClass.con);
+                            cmd1.CommandType = CommandType.Text;
+                            SqlDataReader dr1 = cmd1.ExecuteReader();
+                            if (dr1.Read())
+                            {
+                                if (DateTime.Parse(dr1["Date"].ToString()) <= DateTime.Now.Date)
+                                {
+                                    dr1.Close();
+                                    SqlCommand cmd2 = new SqlCommand("update  example set status = 0", MainClass.con);
+                                    cmd2.CommandType = CommandType.Text;
+                                    cmd2.ExecuteNonQuery();
+                                    MessageBox.Show("Your Software has been expired");
+
+                                    AdminLogin amd = new AdminLogin();
+                                    amd.ShowDialog();
+                                }
+                                else
+                                {
+                                    try
+                                    {
+                                        dr1.Close();
+                                        SqlCommand cmd2 = new SqlCommand("select today from example e where e.status = 1", MainClass.con);
+                                        cmd2.CommandType = CommandType.Text;
+                                        SqlDataReader dr2 = cmd2.ExecuteReader();
+                                        if (dr2.Read())
+                                        {
+                                            if (DateTime.Parse(dr2["today"].ToString()) > DateTime.Now.Date)
+                                            {
+                                                SqlCommand cmd3 = new SqlCommand("update  example set status = 0", MainClass.con);
+                                                cmd3.CommandType = CommandType.Text;
+                                                cmd3.ExecuteNonQuery();
+                                                MessageBox.Show("Your Software has been expired");
+                                                AdminLogin amd = new AdminLogin();
+                                                amd.ShowDialog();
+                                            }
+                                            else
+                                            {
+                                                MainClass.con.Close();
+                                                try
+                                                {
+                                                    SqlCommand cmd4 = new SqlCommand("update example set today = '" + DateTime.Now.Date + "' where status = 1", MainClass.con);
+                                                    cmd4.CommandType = CommandType.Text;
+                                                    MainClass.con.Open();
+                                                    cmd4.ExecuteNonQuery();
+                                                    MainClass.con.Close();
+                                                }
+                                                catch (Exception ex)
+                                                {
+                                                    MainClass.con.Close();
+                                                    MessageBox.Show(ex.Message);
+                                                }
+
+                                                LoginScreen hs = new LoginScreen();
+                                                MainClass.showWindow(hs, this);
+                                            }
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        MainClass.con.Close();
+                                        MessageBox.Show(ex.Message);
+                                    }
+
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            AdminLogin amd = new AdminLogin();
+                            amd.ShowDialog();
+                        }
+
+                        MainClass.con.Close();
+
+                    }
+                    else
+                    {
+                        AdminLogin amd = new AdminLogin();
+                        amd.ShowDialog();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MainClass.con.Close();
+                    MessageBox.Show(ex.Message);
+                }
+
+
+
+                
+            }
+            else
+            {
+                DatabaseSettings sl = new DatabaseSettings();
+                sl.ShowDialog();
+            }
         }
     }
 }
